200fb3e9ebfc60c2f52349d41762464194475a2a
DenilsonMelo <denilsonmlo42@gmail.com>
2024-03-20 20:19:24 -0300

    adicionado o consumo de api

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,11 +1,28 @@
-
+import { useEffect, useState } from 'react';
 import './App.css'
 import Header from './components/Header'
 
 function App() {
+  const [todo, setTodo] = useState([]);
+
+  useEffect(() => {
+    async function fetchData(){
+      try{
+        const response = await fetch('http://localhost:8080/todo')
+        const data = await response.json();
+        setTodo(data);
+      } catch(err) {
+        throw new Error("erro: ", err);
+      }
+    }
+
+    fetchData();
+  }, [])
+
+  console.log(todo);
   return (
     <>
-        <Header />
+       <Header />
     </>
   )
 }
